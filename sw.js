837175558f8f4a81cd486dd9f5aaca19a81b6900
craigoriusgif <mailclh@gmail.com>
2025-12-21 09:56:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,29 @@
-const CACHE_NAME = 'offline-v1';
-const ASSETS_TO_CACHE = [
-  './',              // The main page
+const CACHE_NAME = 'glossearch-v2';
+const ASSETS = [
+  './',
   './index.html',
-  './style.css',     // Change these to your actual filenames
-  './script.js',
-  './icon.png'
+  './manifest.json',
+  './logo2.png',
+  './Magglasslogo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/list.js/2.3.1/list.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js'
 ];
 
-// Install: Save files to cache
+// Install: Cache all files
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Activate: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
